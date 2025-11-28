13ffe537f4c96a225d5f84b41f380ba5306bef2a
frlvoleg <84570176+frlvoleg@users.noreply.github.com>
2025-11-28 16:09:32 +0200

    close icon for the header banner

--- src/shared/ui/HeaderBanner/HeaderBanner.tsx
+++ src/shared/ui/HeaderBanner/HeaderBanner.tsx
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import hotfire_telephone from "../../assets/images/png/hotfire3.png";
 
 import s from "./HeaderBanner.module.scss";
+import { CloseIcon } from "../../assets/images/svg/CloseIcon";
 
 const HeaderBanner = () => {
   return (
@@ -17,8 +18,12 @@ const HeaderBanner = () => {
             <span> Contact Us For Expedited Service</span>
           </span>
         </span>
-        <span>+1 631-910-6906</span>
+        <span className={s.headerPhone}>+1 631-910-6906</span>
       </Link>
+
+      <div className={s.closeIcon}>
+        <CloseIcon />
+      </div>
     </div>
   );
 };
