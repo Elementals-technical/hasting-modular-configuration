1b8d137788b4e80b130d450fe48385e6d2e7f4d9
frlvoleg <84570176+frlvoleg@users.noreply.github.com>
2025-12-02 01:02:36 +0200

    open animation for the sidebar/ get the current path name for the step navigation

--- src/shared/ui/StepNavigationBar /StepNavigationBar.tsx
+++ src/shared/ui/StepNavigationBar /StepNavigationBar.tsx
@@ -3,13 +3,17 @@ import { HintIcon } from "@/shared/assets/images/svg/HintIcon.tsx";
 
 import s from "./StepNavigationBar.module.scss";
 
-export const StepNavigationBar = () => {
+interface StepNavigationBarI {
+  title: string | null;
+}
+
+export const StepNavigationBar: React.FC<StepNavigationBarI> = ({ title }) => {
   return (
     <div className={s.stepNavigationBar}>
       <div>
         <ArrowLeft />
       </div>
-      <div className={s.stepNavigationBar_title}>Select model</div>
+      <div className={s.stepNavigationBar_title}>Select {title}</div>
       <div>
         <HintIcon />
       </div>
--- src/widgets/ConfiguratorSidebar/ui/ConfiguratorSidebar.tsx
+++ src/widgets/ConfiguratorSidebar/ui/ConfiguratorSidebar.tsx
@@ -1,8 +1,13 @@
-import type { PropsWithChildren } from "react";
+import { useEffect, type PropsWithChildren } from "react";
+import { useLocation } from "react-router-dom";
 
 import { BottomStickyBar } from "@/features";
+import { getActiveStep } from "@/features/sidebar/model/store/selectors";
+import { setActiveStep } from "@/features/sidebar/model/store/slice";
 
 import { StepNavigationBar } from "@/shared/ui/StepNavigationBar /StepNavigationBar";
+import { useAppDispatch, useAppSelector } from "@/shared/hooks/store/redux";
+import { CUSTOM_STEPS, PREBUILT_STEPS } from "@/shared/config/steps";
 
 import s from "./ConfiguratorSidebar.module.scss";
 
@@ -11,9 +16,22 @@ type ConfiguratorSidebarProps = PropsWithChildren<{
 }>;
 
 export const ConfiguratorSidebar = ({ flow = "prebuilt", children }: ConfiguratorSidebarProps) => {
+  const location = useLocation();
+
+  const activeStep = useAppSelector(getActiveStep);
+  const dispatch = useAppDispatch();
+
+  const steps = flow === "custom" ? CUSTOM_STEPS : PREBUILT_STEPS;
+
+  useEffect(() => {
+    const match = steps.find((s) => location.pathname.startsWith(s.path));
+
+    dispatch(setActiveStep(match?.label ?? null));
+  }, [dispatch, location.pathname, steps]);
+
   return (
     <div className={s.configSidebar} data-flow={flow}>
-      <StepNavigationBar />
+      <StepNavigationBar title={activeStep} />
 
       <div className={s.stepContent}>{children}</div>
 
