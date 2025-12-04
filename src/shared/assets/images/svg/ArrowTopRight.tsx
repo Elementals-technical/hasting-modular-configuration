eb1f342d9c50d78e3858271db8777d5bddbda750
frlvoleg <84570176+frlvoleg@users.noreply.github.com>
2025-12-04 11:07:32 +0200

    change ui

--- src/shared/assets/images/svg/ArrowTopRight.tsx
+++ src/shared/assets/images/svg/ArrowTopRight.tsx
@@ -1,9 +1,13 @@
-export const ArrowTopRight = () => {
+interface ArrowTopRightI {
+  color?: string;
+}
+
+export const ArrowTopRight: React.FC<ArrowTopRightI> = ({ color = "white" }) => {
   return (
     <svg width="15" height="15" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
       <path
         d="M3.5 3.5H8.5M8.5 3.5V8.5M8.5 3.5L3.5 8.5"
-        stroke="white"
+        stroke={color}
         stroke-linecap="round"
         stroke-linejoin="round"
       />
