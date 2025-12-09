5ac2ec4f998d837491d975c6148481c4a9d47d49
frlvoleg <84570176+frlvoleg@users.noreply.github.com>
2025-12-09 12:03:52 +0200

    cabinet styles items

---
+++ src/entities/product/ui/ProductStyleGrid/ProductStyleGrid.tsx
@@ -0,0 +1,35 @@
+import { ProductStyleItem } from "@/shared/ui/ProductStyleItem/ProductStyleItem";
+import { useAppSelector } from "@/shared/hooks/store/redux";
+
+import { getActiveCabinetType } from "../../model/store/selectors";
+
+import s from "./ProductStyleGrid.module.scss";
+
+interface ProductStyleGridI {
+  data: {
+    id: number;
+    title: string;
+    name?: string | undefined;
+    desc?: string | undefined;
+    isAvailable?: boolean;
+    isShortDesc: boolean;
+  }[];
+  requiresActiveCabinet?: boolean;
+}
+
+export const ProductStyleGrid: React.FC<ProductStyleGridI> = ({ data, requiresActiveCabinet }) => {
+  const activeCabinet = useAppSelector(getActiveCabinetType);
+  const hasActiveCabinet = activeCabinet !== null;
+
+  if (requiresActiveCabinet && !hasActiveCabinet) {
+    return <div className={s.message}>Select cabinet type first</div>;
+  }
+
+  return (
+    <div className={s.optionsGrid}>
+      {data.map((i) => (
+        <ProductStyleItem key={i.id} id={i.id} title={i.title} />
+      ))}
+    </div>
+  );
+};
---
+++ src/shared/ui/ProductStyleItem/ProductStyleItem.tsx
@@ -0,0 +1,29 @@
+import { Link } from "react-router-dom";
+
+import { ArrowTopRight } from "@/shared/assets/images/svg/ArrowTopRight";
+import none_img from "../../assets/images/png/img_png.png";
+
+import s from "./ProductStyleItem.module.scss";
+
+interface ProductStyleItemI {
+  id: number;
+  title: string;
+}
+
+export const ProductStyleItem: React.FC<ProductStyleItemI> = ({ id, title }) => {
+  return (
+    <div className={s.productStyleItem}>
+      <div className={s.image}>
+        <img src={none_img} alt="image" />
+      </div>
+      <div className={s.title}>{title}</div>
+
+      <Link className={s.link} to={`#`}>
+        <span>Product Details</span>
+        <span className={s.linkIcon}>
+          <ArrowTopRight color={"#ad5534"} />
+        </span>
+      </Link>
+    </div>
+  );
+};
