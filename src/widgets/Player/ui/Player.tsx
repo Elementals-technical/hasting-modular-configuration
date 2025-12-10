2cdbca6bcc0d4e4b6deff055989990b18410394a
frlvoleg <84570176+frlvoleg@users.noreply.github.com>
2025-12-10 12:09:30 +0200

    canvas icons at the bottom

--- src/widgets/Player/ui/Player.tsx
+++ src/widgets/Player/ui/Player.tsx
@@ -1,9 +1,12 @@
+import { BottomCanvasButtons } from "@/features/bottomCanvasButtons/bottomCanvasButtons";
 import { PlayCanvasIntegration } from "@/widgets/Player/components/PlayCanvasIntegration/PlayCanvasIntegration.tsx";
 
 export function Player() {
   return (
     <div>
       <PlayCanvasIntegration />
+
+      <BottomCanvasButtons />
     </div>
   );
 }
