dbe0a7fe023200fca97741c48d1ee5814c66ac3b
frlvoleg <84570176+frlvoleg@users.noreply.github.com>
2025-12-16 15:35:46 +0200

    updated canvas/reset scene btn

--- src/features/bottomCanvasButtons/BottomCanvasButtons.tsx
+++ src/features/bottomCanvasButtons/BottomCanvasButtons.tsx
@@ -7,8 +7,65 @@ import { ShareIcon } from "@/shared/assets/images/svg/ShareIcon";
 import { RotateIcon } from "@/shared/assets/images/svg/RotateIcon";
 
 import s from "./BottomCanvasButtons.module.scss";
+import { removeAllProducts } from "@/utils/functions/playcanvas/removeAllProducts";
+import {
+  addProductId,
+  resetProducts,
+  setActiveBasinStyle,
+  setActiveCabinetType,
+  setDrawerProduct,
+  setSelectedDimensions,
+  setSelectedProductConfig,
+} from "@/entities/product/model/store/slice";
+import { addProduct, type addProductConfigI } from "@/utils/functions/playcanvas/addProduct";
+import { useAppDispatch } from "@/shared/hooks/store/redux";
+import { optionsMockData } from "@/pages/custom/cabinetBuilder/constants";
 
 export const BottomCanvasButtons = () => {
+  const dispatch = useAppDispatch();
+
+  const resetCustomBuilderScene = async () => {
+    removeAllProducts();
+    dispatch(resetProducts());
+
+    const firstCabinetOption = optionsMockData[0];
+
+    const defaultProductName = "Sink-Base";
+    const defaultProductConfig: addProductConfigI = {
+      Height: 56,
+      Depth: 46,
+      CabinetColor: "Ardesia DD GL",
+      Width: 60,
+      sinkType: "Top_HPLPrisma",
+      CountertopColor: "Rosso Rubino 19 MT",
+      HandleGrooveColor: "Blu Pavone A6 MT",
+    };
+
+    if (firstCabinetOption) {
+      dispatch(setActiveCabinetType(firstCabinetOption.id));
+
+      const productId = await addProduct(defaultProductName, defaultProductConfig);
+
+      dispatch(setDrawerProduct(defaultProductName));
+      dispatch(setSelectedProductConfig(defaultProductConfig));
+      dispatch(
+        setSelectedDimensions({
+          width: defaultProductConfig.Width,
+          height: defaultProductConfig.Height,
+          depth: defaultProductConfig.Depth,
+        }),
+      );
+
+      if (defaultProductConfig.sinkType) {
+        dispatch(setActiveBasinStyle(defaultProductConfig.sinkType));
+      }
+
+      if (productId) {
+        dispatch(addProductId(productId));
+      }
+    }
+  };
+
   return (
     <div className={s.bottomCanvasButtons}>
       <BaseButton variant="ghost">
@@ -31,7 +88,7 @@ export const BottomCanvasButtons = () => {
         <ShareIcon />
       </BaseButton>
 
-      <BaseButton variant="ghost">
+      <BaseButton variant="ghost" onClick={resetCustomBuilderScene}>
         <RotateIcon />
       </BaseButton>
     </div>
