f62b3f5407803303017515137fc423a1dc4e4954
frlvoleg <84570176+frlvoleg@users.noreply.github.com>
2025-12-23 15:02:21 +0200

    update playcanvas

--- src/widgets/Player/components/PlayCanvasIntegration/PlayCanvasIntegration.tsx
+++ src/widgets/Player/components/PlayCanvasIntegration/PlayCanvasIntegration.tsx
@@ -32,7 +32,7 @@ import { setConfigBatch } from "@/utils/functions/playcanvas/setConfigBatch";
 import { getOrderedProductIds } from "@/utils/functions/playcanvas/getOrderedProductIds";
 
 // 🔧 UPDATE THIS VERSION WHEN DEPLOYING NEW PLAYCANVAS BUILD
-const PLAYCANVAS_VERSION = "005";
+const PLAYCANVAS_VERSION = "006";
 const PLAYCANVAS_SRC = `/HastingCabinetsParametrization/index.html?v=${PLAYCANVAS_VERSION}`;
 // const RIGHT_BUTTON = 2;
 // const HOLD_MS = 250;
