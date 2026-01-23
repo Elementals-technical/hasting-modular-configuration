4a70facece33994fe5c20ae6a6e0510b355416ac
frlvoleg <84570176+frlvoleg@users.noreply.github.com>
2026-01-23 23:49:38 +0200

    setVisibleDrawerButtons method

--- src/utils/functions/playcanvas/setVisibleDrawerButtons.ts
+++ src/utils/functions/playcanvas/setVisibleDrawerButtons.ts
@@ -3,26 +3,20 @@ export function setVisibleDrawerButtons(isEnabled: boolean) {
   const containerRef = window.containerRef;
   const canvasIframe = containerRef?.current?.contentWindow as any;
 
-  const setVisibleDrawerButtons =
-    canvasIframe?.ConfiguratorAPI?.setVisibleDrawerButtons;
+  const setVisibleDrawerButtons = canvasIframe?.ConfiguratorAPI?.setVisibleDrawerButtons;
 
   console.log("call setVisibleDrawerButtons", setVisibleDrawerButtons);
   console.log("isEnabled setVisibleDrawerButtons", isEnabled);
 
   if (!setVisibleDrawerButtons) {
-    console.warn(
-      "[PlayCanvas] ConfiguratorAPI.setVisibleDrawerButtons not ready"
-    );
+    console.warn("[PlayCanvas] ConfiguratorAPI.setVisibleDrawerButtons not ready");
     return null;
   }
 
   try {
     setVisibleDrawerButtons(isEnabled);
   } catch (error) {
-    console.error(
-      "[PlayCanvas] Failed to set setVisibleDrawerButtons",
-      error
-    );
+    console.error("[PlayCanvas] Failed to set setVisibleDrawerButtons", error);
     return null;
   }
 }
