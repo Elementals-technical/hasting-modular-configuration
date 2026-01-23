6c0398c95f6e46eeb35fdd6efa9a7476f1656cc1
frlvoleg <84570176+frlvoleg@users.noreply.github.com>
2026-01-23 13:19:44 +0200

    base steps implementation

---
+++ src/shared/ui/Popups/ui/HelpPopup/steps/assistance/assistance.tsx
@@ -0,0 +1,8 @@
+export const AssistanceStep = () => {
+  return (
+    <div>
+      <div>Design assistance</div>
+      <p>Tell us about your space and what you want to achieve.</p>
+    </div>
+  );
+};
---
+++ src/shared/ui/Popups/ui/HelpPopup/steps/question/question.tsx
@@ -0,0 +1,8 @@
+export const QuestionStep = () => {
+  return (
+    <div>
+      <div>Questions</div>
+      <p>Share your question and we will help you out.</p>
+    </div>
+  );
+};
