9c85f1e500751c96763908a7ceb3322778df6b83
frlvoleg <84570176+frlvoleg@users.noreply.github.com>
2026-02-11 11:14:23 +0200

    presets filters

--- src/entities/product/types.ts
+++ src/entities/product/types.ts
@@ -11,6 +11,16 @@ export type PresetProduct = {
   HandleGrooveColor?: string;
 };
 
+export type ProductSize = "24_29" | "30_39" | "40_49" | "50_59" | "60_69" | "70_79" | "80_89" | "90_plus";
+
+export type ProductStyle =
+  | "1_drawer"
+  | "2_drawer"
+  | "single_basin"
+  | "double_basin"
+  | "asymmetrical"
+  | "open_shelving";
+
 export type ProductModel = {
   id: number;
   img: string;
@@ -19,4 +29,6 @@ export type ProductModel = {
   isProductModel: boolean;
   price?: string;
   presetProducts: PresetProduct[];
+  size: ProductSize;
+  style: ProductStyle[];
 };
--- src/features/filters/ui/filterItem/FilterItem.tsx
+++ src/features/filters/ui/filterItem/FilterItem.tsx
@@ -3,13 +3,14 @@ import { FilterSelection } from "@/shared/ui/Filter/FilterSelection";
 interface FilterItemI {
   label: string;
   options: { label: string; value: string }[];
+  value?: string | number;
   onSelect?: (value: string | number) => void;
 }
 
-export const FilterItem: React.FC<FilterItemI> = ({ label, options, onSelect }) => {
-  const handleSelect = (value: string | number) => {
-    onSelect?.(value);
+export const FilterItem: React.FC<FilterItemI> = ({ label, options, value, onSelect }) => {
+  const handleSelect = (val: string | number) => {
+    onSelect?.(val);
   };
 
-  return <FilterSelection label={label} options={options} onSelect={handleSelect} />;
+  return <FilterSelection label={label} options={options} value={value} onSelect={handleSelect} />;
 };
