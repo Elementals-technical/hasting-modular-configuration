35390cb0af8615a7f423944b12cc66b3ff6a5539
frlvoleg <84570176+frlvoleg@users.noreply.github.com>
2026-02-20 10:09:35 +0200

    Refactor HelpPopup component to remove routing logic and replace links with disabled actions

--- src/shared/ui/Popups/ui/HelpPopup/HelpPopup.tsx
+++ src/shared/ui/Popups/ui/HelpPopup/HelpPopup.tsx
@@ -1,12 +1,8 @@
-import { Link, useSearchParams } from "react-router-dom";
-
 import { BaseButton } from "@/shared";
 import { CloseBtnIcon } from "@/shared/assets/images/svg/CloseBtnIcon";
 import { ArrowRight } from "@/shared/assets/images/svg/ArrowRight";
 
 import { PopupRightContent } from "../../PopupRightContent/PopupRightContent";
-import { AssistanceStep } from "./steps/assistance/assistance";
-import { QuestionStep } from "./steps/question/question";
 
 import s from "./HelpPopup.module.scss";
 
@@ -16,34 +12,11 @@ interface HelpPopupI {
 }
 
 export const HelpPopup: React.FC<HelpPopupI> = ({ isOpening, onClose }) => {
-  const [searchParams] = useSearchParams();
-  const step = searchParams.get("step") || "";
-  const stepsMap: Record<string, React.FC> = {
-    assistance: AssistanceStep,
-    design: AssistanceStep,
-    question: QuestionStep,
-    product: QuestionStep,
-    general: QuestionStep,
+  const handleDisabledLink = (event: React.MouseEvent<HTMLAnchorElement>) => {
+    event.preventDefault();
   };
-  const StepComponent = stepsMap[step];
-
-  const buildStepLink = (step: string) => {
-    const nextParams = new URLSearchParams(searchParams);
-    nextParams.set("help", "1");
-    nextParams.set("step", step);
-    const search = nextParams.toString();
-
-    return {
-      search: search ? `?${search}` : "",
-    };
-  };
-
   return (
-    <PopupRightContent
-      onClose={onClose}
-      isOpening={isOpening}
-      animationDurationMs={400}
-    >
+    <PopupRightContent onClose={onClose} isOpening={isOpening} animationDurationMs={400}>
       <div className={s.instrPopup}>
         <div className={s.header}>
           <div className={s.title}>How can we help?</div>
@@ -53,52 +26,48 @@ export const HelpPopup: React.FC<HelpPopupI> = ({ isOpening, onClose }) => {
         </div>
 
         <div className={s.content}>
-          {StepComponent ? (
-            <StepComponent />
-          ) : (
-            <ul className={s.popupList}>
-              <li>
-                <Link to={buildStepLink("question")}>
-                  <span>I have a product question</span>
-                  <span>
-                    <ArrowRight />
-                  </span>
-                </Link>
-              </li>
-              <li>
-                <Link to={buildStepLink("assistance")}>
-                  <span>I need design assistance</span>
-                  <span>
-                    <ArrowRight />
-                  </span>
-                </Link>
-              </li>
-              <li>
-                <Link to={buildStepLink("question")}>
-                  <span>Order Free Samples</span>
-                  <span>
-                    <ArrowRight />
-                  </span>
-                </Link>
-              </li>
-              <li>
-                <Link to={buildStepLink("question")}>
-                  <span>View In My Space</span>
-                  <span>
-                    <ArrowRight />
-                  </span>
-                </Link>
-              </li>
-              <li>
-                <Link to={buildStepLink("question")}>
-                  <span>Question</span>
-                  <span>
-                    <ArrowRight />
-                  </span>
-                </Link>
-              </li>
-            </ul>
-          )}
+          <ul className={s.popupList}>
+            <li>
+              <a href="#" onClick={handleDisabledLink}>
+                <span>I have a product question</span>
+                <span>
+                  <ArrowRight />
+                </span>
+              </a>
+            </li>
+            <li>
+              <a href="#" onClick={handleDisabledLink}>
+                <span>I need design assistance</span>
+                <span>
+                  <ArrowRight />
+                </span>
+              </a>
+            </li>
+            <li>
+              <a href="#" onClick={handleDisabledLink}>
+                <span>Order Free Samples</span>
+                <span>
+                  <ArrowRight />
+                </span>
+              </a>
+            </li>
+            <li>
+              <a href="#" onClick={handleDisabledLink}>
+                <span>View In My Space</span>
+                <span>
+                  <ArrowRight />
+                </span>
+              </a>
+            </li>
+            <li>
+              <a href="#" onClick={handleDisabledLink}>
+                <span>Question</span>
+                <span>
+                  <ArrowRight />
+                </span>
+              </a>
+            </li>
+          </ul>
         </div>
 
         <div className={s.footer}>
