335de40df7530885884d96fac135fa7be0cb8738
frlvoleg <84570176+frlvoleg@users.noreply.github.com>
2026-03-13 19:17:02 +0200

    how to popup

--- src/shared/assets/images/svg/CloseIcon.tsx
+++ src/shared/assets/images/svg/CloseIcon.tsx
@@ -1,8 +1,12 @@
-export const CloseIcon = () => {
+type CloseIconProps = {
+  fill?: string;
+};
+
+export const CloseIcon = ({ fill = "white" }: CloseIconProps) => {
   return (
     <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <path d="M5.99805 6.00293L17.998 18.0029" stroke="white" stroke-width="1.06066"></path>
-      <path d="M18.002 6.00098L6.00195 18.001" stroke="white" stroke-width="1.06066"></path>
+      <path d="M5.99805 6.00293L17.998 18.0029" stroke={fill} strokeWidth="1.06066"></path>
+      <path d="M18.002 6.00098L6.00195 18.001" stroke={fill} strokeWidth="1.06066"></path>
     </svg>
   );
 };
