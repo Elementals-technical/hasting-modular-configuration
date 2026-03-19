8eb3b70a8eb2fc991162964fda58fb491adaeaa4
frlvoleg <84570176+frlvoleg@users.noreply.github.com>
2026-03-19 11:09:06 +0200

    updated arrow for the nested menu

---
+++ src/shared/assets/images/svg/ArrowRightNestedIcon.tsx
@@ -0,0 +1,7 @@
+export const ArrowRightNestedIcon = () => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 12 12" fill="none">
+      <path d="M4.5 9L7.5 6L4.5 3" stroke="#282828" stroke-linecap="round" stroke-linejoin="round" />
+    </svg>
+  );
+};
